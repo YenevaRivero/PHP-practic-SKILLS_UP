ad0ad901c4dab97aa809209717e8ca67c3317c30
YenevaR <yeneva.pfaeskillsup@gmail,com>
2022-09-27 09:59:49 +0100

    Reorganización de carpetas, creación de archivo.php, añadir action al form enlazando el archivo new_reg

---
+++ modules/include/news_reg.php
@@ -0,0 +1,11 @@
+<?php
+require "../require/config.php";
+
+$nombre = $email = $telefono = $direccion = $ciudad = $provincia = $zip = $check = $noticia =$otrostemas;
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    
+}
+
+?>
