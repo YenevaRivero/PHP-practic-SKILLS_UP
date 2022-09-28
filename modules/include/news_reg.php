5985649f4e6506a6703b47761ac1449c76e6fdcd
YenevaR <yeneva.pfaeskillsup@gmail,com>
2022-09-28 10:30:53 +0100

    Omitido el botón enviar, Poner campo requerido en el telefono y un placeholder explicando que es requerido

--- modules/include/news_reg.php
+++ modules/include/news_reg.php
@@ -1,11 +1,15 @@
 <?php
 require "../require/config.php";
 
-$nombre = $email = $telefono = $direccion = $ciudad = $provincia = $zip = $check = $noticia =$otrostemas;
+$nombre = $email = $telefono = $direccion = $ciudad = $provincia = $zip = $check = $noticia =$otrostemas="";
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-    
+    if(!empty($_POST["name"])){
+        echo "<br><strong>Metodo post enviado</strong><br>";
+
+    }
+
 }
 
 ?>
