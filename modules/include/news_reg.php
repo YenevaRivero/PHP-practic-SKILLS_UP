4bcdf57b55df94dfaab807cafdf8a9b177d2ba34
YenevaR <yeneva.pfaeskillsup@gmail,com>
2022-09-29 11:52:58 +0100

    Comprobación de validaciones y limpiar datos

--- modules/include/news_reg.php
+++ modules/include/news_reg.php
@@ -3,58 +3,81 @@ require "../require/config.php";
 
 $nombre = $email = $telefono = $direccion = $ciudad = $provincia = $zip = $check = $noticia =$otrostemas="";
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-    if(!empty($_POST["nombre"]) || !empty($_POST["email"]) || !empty($_POST["telefono"])){
-        echo "<br><strong>Metodo post enviado</strong><br>";
-        $nombre= $_POST["nombre"];
-        $email= $_POST["email"];
-        $telefono= $_POST["telefono"];
-        $direccion= $_POST["direccion"];
-        $ciudad= $_POST["ciudad"];
-        $provincia= $_POST["provincia"];
-        $zip= $_POST["zip"];
-        $check= $_POST["check"];
-        $noticia= $_POST["noticia"];
-        $otrostemas= $_POST["otrostemas"];
-
         function limpiar_dato($data){
             $data = trim($data); // Elimina espacio en blanco (u otro tipo de caracteres) del inicio o final de la cadena
             $data = stripslashes($data); // Devuelve cadenas con las barras invertidas o comillas borradas.
             $data = htmlspecialchars($data); //Limpiar caracteres especiales.
+            return $data; //Devuelve el valor de las variables (lo que escribimos en los inputs)
         }
 
-        //nombre, email y número de teléfono
 
-    function validar_nombre($nombre){
-        
-        if (!preg_match("/^[a-zA-Z-' ]*$/",$nombre)) {
-            return false;
 
-            }  else{
-                return true;
-            }
-        }
-
-        function validar_telefono($telefono){
-            if(!preg_match('/^[0-9]{10}+$/', $telefono)){
+        function validar_nombre($nombre){
+            
+            if (!preg_match("/^[a-zA-Z-' ]*$/",$nombre)) {
                 return false;
-            } else{
-                return true;
+    
+                }  else{
+                    return true;
+                }
             }
-
-        }
-
-        function validar_email($email){
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                return false;
+    
+            function validar_telefono($telefono){
+                if(!preg_match('/^[0-9]{10}+$/', $telefono)){
+                    return false;
                 } else{
                     return true;
                 }
+    
+            }
+    
+            function validar_email($email){
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    return false;
+                    } else{
+                        return true;
+                    }
+            }
+        
+    
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    print_r ($_POST);
+    if(!empty($_POST["nombre"]) || !empty($_POST["email"]) || !empty($_POST["telefono"])){
+        echo "<br><strong>Metodo post enviado</strong><br>";
+        $nombre= limpiar_dato($_POST["nombre"]);
+        echo "<strong>Nombre: </strong>" . $nombre . "<br>";
+        $email= limpiar_dato($_POST["email"]);
+        echo "<strong>Email: </strong>" . $email . "<br>";
+        $telefono= limpiar_dato($_POST["telefono"]);
+        echo "<strong>Telefono: </strong>" . $telefono . "<br>";
+        $direccion= limpiar_dato($_POST["direccion"]);
+        $ciudad= limpiar_dato($_POST["ciudad"]);
+        $provincia= limpiar_dato($_POST["provincia"]);
+        $zip= limpiar_dato($_POST["zip"]);
+        $check= limpiar_dato($_POST["check"]);
+        $noticia= limpiar_dato($_POST["noticia"]);
+        $otrostemas= limpiar_dato($_POST["otrostemas"]);
+
+
+        if(validar_nombre($nombre)){
+            echo"validada";
+        } else{
+            echo"no valida";
         }
+/* revisar validaciones
+        if(validar_email($email)){
+            echo"validada";
+        } else{
+            echo"no valida";
+        }
+        
+        if(validar_telefono($telefono)){
+            echo"validada";
+        } else{
+            echo"no valida";
+        }
+*/
     }
 
-
 }
-
 ?>
