25331dca36718501b3fbf4d619a31db3dc8c1a75
YenevaR <yeneva.pfaeskillsup@gmail,com>
2022-10-17 09:44:56 +0100

    Convertir valor de un array en el formato binario de la base de datos

--- modules/include/news_reg.php
+++ modules/include/news_reg.php
@@ -3,6 +3,7 @@ require "../require/config.php";
 
 $nombre = $email = $telefono = $direccion = $ciudad = $provincia = $zip = $check = $noticia = $otrostemas="";
 $nombre_err = $email_err = $telefono_err = false;
+$checkboxs;
 
 function limpiar_dato($data){
     $data = trim($data); // Elimina espacio en blanco (u otro tipo de caracteres) del inicio o final de la cadena
@@ -49,6 +50,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         echo "<strong>Email: </strong>" . $email . "<br>";
         $telefono= limpiar_dato($_POST["telefono"]);
         echo "<strong>Telefono: </strong>" . $telefono . "<br>";
+
+
 
         if(validar_nombre($nombre)){
             echo"validada";
@@ -116,6 +119,38 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
            /// echo "<br>";
             
+            $lengArray = count($check);
+
+            switch ($lengArray){
+                case 1:
+                    if($check=[0] == "HTML News"){
+                            $checkboxs = 100;
+                    } elseif($check[0] == "CSS News"){
+                            $checkboxs = 010;
+                    } else{
+                            $checkboxs = 001;
+                    }
+                    break;
+                case 2:
+                    if($check[0] != "HTML News"){
+                            $checkboxs = 011;
+                        } elseif($check[0] != "CSS News"){
+                            $checkboxs = 101;
+                        } else {
+                            $checkboxs = 110;
+                        }
+
+                    break;
+                case 3:
+                    $checkboxs = 111 ;
+                    break;
+                default: 
+                    $checkboxs = 100;
+            }
+
+            echo "valor a devolver " . $checkboxs ."<br>";
+
+
             // === Usa un array y muestra sus valores separados por coma (o lo que se ponga entre comillas).
 
             $string = implode(", ", $check);
@@ -157,9 +192,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             echo "<br><strong>Noticia:</strong> $noticia <br>";
             echo "<br><strong>Otros temas:</strong> $otrostemas <br>";
 
+            //Comprobar que no existen datos que se van a enviar: nombre, email y telefono.
 
+            SELECT fullname, email, phone FROM news_reg WHERE $nombre="fullname", $email="email", $telefono="phone";
+            
+            //Si devuelve algo, que
+            // INSERT datos a la base de datos;
+
+            INSERT INTO fullname, email, phone, address, city, state, zipcode, newsletters, format_news, suggestion VALUES ($nombre,  $email,  $telefono,  $direccion,  $ciudad,  $provincia,  $zip,  $check,  $noticia, $otrostemas);
+            
         } else{
-        
+            echo "Una de las validaciones ha fallado. </br>";
             if ($nombre_err == true){
                 echo "La validación del nombre ha fallado";
             }elseif($email_err == true){
